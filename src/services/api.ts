7c5ec3b61f5441f9ced646c60f45a0276ebf58e5
Karl Belousov <belousovkarl@gmail.com>
2024-11-16 17:18:01 +0600

    добавил модуль для работы  с апи и модуль для работы с токеном

---
+++ src/services/api.ts
@@ -0,0 +1,24 @@
+import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import { getToken } from './token';
+
+const BACKEND_URL = 'https://grading.design.htmlacademy.pro/v0/keks';
+const REQUEST_TIMEOUT = 5000;
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
+      if (token && config.headers) {
+        config.headers['X-token'] = token;
+      }
+      return config;
+    },
+  );
+
+  return api;
+};
